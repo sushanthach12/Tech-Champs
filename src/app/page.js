ffaaa238913b979446caa3bee65f0616e0277e49
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-06-02 17:02:50 +0530

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <main className="bg-white">
 
-   <div class="relative isolate px-6 lg:px-8">
-      <div class="relative isolate px-6 lg:px-8">
+   <div class="relative px-6 lg:px-8">
+      <div class="relative px-6 lg:px-8">
         <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
           <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg]  opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" ></div>
         </div>
