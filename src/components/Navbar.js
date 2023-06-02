e19378a1309789e6b8015c408cd325e4c5e96e69
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-06-02 16:34:32 +0530

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 "use client";
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import React from "react";
